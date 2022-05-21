533a4138fb95796a23afe1ea067b674f65e27cb1
fabiosdiasrs <fabiosdiasrs@gmail.com>
2022-05-21 11:30:56 -0300

    #5 eventos JSON

--- config/config.php
+++ config/config.php
@@ -5,7 +5,6 @@ define('DIRPAGE',"http://{$_SERVER['HTTP_HOST']}/{$dirInt}");
 $bar      = (substr($_SERVER['DOCUMENT_ROOT'],-1)=='/')?"":"/"; # caso no servidor o último caracter do Cocument_root for uma barra não adiciona nada caso contrário adiciona a barra
 define('DIRREQ',"{$_SERVER['DOCUMENT_ROOT']}{$bar}{$dirInt}"); # constante para incluir arquivos 
 
-echo DIRPAGE. '<br>' . DIRREQ; 
 #Banco de Dados
 define('HOST','localhost');
 define('DB','sistema');
--- index.php
+++ index.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Agendamento de Consultas</title>
+    <link rel="stylesheet" href="<?php echo DIRPAGE.'lib/css/style.css';?>">
+    <link rel="stylesheet" href="<?php echo DIRPAGE.'lib/js/FullCalendar/main.min.css';?>">
 </head>
 <body>
 
-<?php
-    $objeto=new Classes\ClassTeste();
-?>
-
+    <div class="calendar"></div>
+    <script src="<?php echo DIRPAGE.'lib/js/FullCalendar/main.min.js';?>"></script>
+    <script src="<?php echo DIRPAGE.'lib/js/javascript.js';?>"></script>
 </body>
 </html>
