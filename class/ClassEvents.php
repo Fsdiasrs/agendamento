6e98a0da7db7cfcdbf287cbb659da50fce73e532
fabiosdiasrs <fabiosdiasrs@gmail.com>
2022-05-21 19:39:59 -0300

    #10 Cadastro de Eventos

--- class/ClassEvents.php
+++ class/ClassEvents.php
@@ -13,5 +13,18 @@
             $f=$b->fetchAll(\PDO::FETCH_ASSOC);
             return json_encode($f);
         }
+
+        #Criação da consulta no banco
+        public function createEvent($id=0,$title,$description,$color='blue',$start,$end)
+        {
+            $b=$this->conectDB()->prepare("insert into events values (?,?,?,?,?,?)");
+            $b->bindParam(1, $id, \PDO::PARAM_INT);
+            $b->bindParam(2, $title, \PDO::PARAM_STR);
+            $b->bindParam(3, $description, \PDO::PARAM_STR);
+            $b->bindParam(4, $color, \PDO::PARAM_STR);
+            $b->bindParam(5, $start, \PDO::PARAM_STR);
+            $b->bindParam(6, $end, \PDO::PARAM_STR);
+            $b->execute();
+        }
     }
 ?>
