8ac23d48ad33ec1d75a48a6c8ae5e6e0d45299d0
fabiosdiasrs <fabiosdiasrs@gmail.com>
2022-05-21 14:16:23 -0300

    #6 Introdução de Eventos com Banco de Dados

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 <body>
 
     <div class="calendar"></div>
+    
     <script src="<?php echo DIRPAGE.'lib/js/FullCalendar/main.min.js';?>"></script>
     <script src="<?php echo DIRPAGE.'lib/js/javascript.js';?>"></script>
 </body>
