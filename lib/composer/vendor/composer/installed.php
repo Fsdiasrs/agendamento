eeb5109b94ce86d98ae832900fd5ab3cfad28aca
fabiosdiasrs <fabiosdiasrs@gmail.com>
2022-05-21 09:31:24 -0300

    #1 Criação do Projeto

---
+++ lib/composer/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c4f006660006bccb421d8253c02c52409bc1055c',
+        'name' => 'pi3a/agendamento_consultas',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pi3a/agendamento_consultas' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c4f006660006bccb421d8253c02c52409bc1055c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
