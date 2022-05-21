951cf8d0288279cf1048d45f50a5516ff7469d8d
fabiosdiasrs <fabiosdiasrs@gmail.com>
2022-05-21 09:36:41 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 #Caminhos absolutos
-$dirInt   = ""; #Caso os arquivos estejam dentro de uma pasta exemplo agendamento/ $dirInt = "agendamento/"
+$dirInt   = "agendamento/"; #Caso os arquivos estejam dentro de uma pasta exemplo agendamento/ $dirInt = "agendamento/"
 define('DIRPAGE',"http://{$_SERVER['HTTP_HOST']}/{$dirInt}");
 $bar      = (substr($_SERVER['DOCUMENT_ROOT'],-1)=='/')?"":"/";
 define('DIRREQ',"{$_SERVER['DOCUMENT_ROOT']}{$bar}{$dirInt}");
