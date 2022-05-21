22fa86689a70473a17b6b83a25ca6ff8b9c3d443
fabiosdiasrs <fabiosdiasrs@gmail.com>
2022-05-21 09:58:28 -0300

    #2

--- config/config.php
+++ config/config.php
@@ -2,9 +2,10 @@
 #Caminhos absolutos
 $dirInt   = "agendamento/"; #Caso os arquivos estejam dentro de uma pasta exemplo agendamento/ $dirInt = "agendamento/"
 define('DIRPAGE',"http://{$_SERVER['HTTP_HOST']}/{$dirInt}");
-$bar      = (substr($_SERVER['DOCUMENT_ROOT'],-1)=='/')?"":"/";
-define('DIRREQ',"{$_SERVER['DOCUMENT_ROOT']}{$bar}{$dirInt}");
+$bar      = (substr($_SERVER['DOCUMENT_ROOT'],-1)=='/')?"":"/"; # caso no servidor o último caracter do Cocument_root for uma barra não adiciona nada caso contrário adiciona a barra
+define('DIRREQ',"{$_SERVER['DOCUMENT_ROOT']}{$bar}{$dirInt}"); # constante para incluir arquivos 
 
+echo DIRPAGE. '<br>' . DIRREQ; 
 #Banco de Dados
 define('HOST','localhost');
 define('DB','sistema');
