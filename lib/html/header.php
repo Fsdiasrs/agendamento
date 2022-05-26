7db36555b77a9fd9d39782096097d17a408571bb
fabiosdiasrs <fabiosdiasrs@gmail.com>
2022-05-26 11:35:48 -0300

    ajustes

--- lib/html/header.php
+++ lib/html/header.php
@@ -6,8 +6,8 @@
     <meta http-equiv    = "X-UA-Compatible" content="ie=edge">
     <title>Agendamento de Consultas</title>
     <link rel           = "stylesheet" href="<?php echo DIRPAGE.'lib/css/style.css';?>">
-    <link rel           = "stylesheet" type="text/css" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel           = "stylesheet" href="<?php echo DIRPAGE.'lib/js/FullCalendar/main.min.css';?>">
 </head>
 <body>
